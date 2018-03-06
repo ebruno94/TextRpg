5566f57e228e00283577edaed60065b1d7a6a8e0
Manuela Garcia <manuela.garcia.dev@gmail.com>
2018-03-06 10:20:06 -0800

    Add CRUD methods to GameUser. Debug CRUD methods.

--- TextRpg/Models/GameUser.cs
+++ TextRpg/Models/GameUser.cs
@@ -64,6 +64,135 @@ namespace TextRpg.Models
             return _email;
         }
 
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO users (name, username, password, email, room_number, character_id) VALUES (@name, @username, @password, @email, @roomNumber, @characterId);";
+
+            MySqlParameter name = new MySqlParameter();
+            name.ParameterName = "@GameUserName";
+            name.Value = _name;
+            cmd.Parameters.Add(name);
+
+            MySqlParameter username = new MySqlParameter();
+            username.ParameterName = "@GameUserUserName";
+            username.Value = _username;
+            cmd.Parameters.Add(username);
+
+            MySqlParameter userPassword = new MySqlParameter("@GameUserPassword", _password);
+            cmd.Parameters.Add(userPassword);
+
+            MySqlParameter email = new MySqlParameter("@GameUserEmail", _email);
+            cmd.Parameters.Add(email);
+
+            MySqlParameter roomNumber = new MySqlParameter("@GameUserRoomNumber", _roomNumber);
+            cmd.Parameters.Add(roomNumber);
+
+            MySqlParameter characterId = new MySqlParameter("@GameUserCharacterId", _character.GetId());
+            cmd.Parameters.Add(characterId);
+
+            cmd.ExecuteNonQuery();
+
+            _id = (int) cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+        public static GameUser Find(int userId)
+        {
+          MySqlConnection conn = DB.Connection();
+          conn.Open();
+          MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+          cmd.CommandText = @"SELECT * FROM users WHERE id=@userId;";
+
+          MySqlParameter searchId = new MySqlParameter();
+          searchId.ParameterName = "@userId";
+          searchId.Value = userId;
+          cmd.Parameters.Add(searchId);
+
+          MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+          int id = 0;
+          string name = "";
+          string username = "";
+          string password = "";
+          string email = "";
+          int roomNumber = 0;
+          while (rdr.Read())
+          {
+            id = rdr.GetInt32(0);
+            username = rdr.GetString(1);
+            password = rdr.GetString(2);
+            name = rdr.GetString(3);
+            email = rdr.GetString(4);
+            roomNumber = Int32.Parse(rdr.GetString(5));
+          }
+
+          GameUser myUser = new GameUser(name, username, password, email, roomNumber);
+          myUser.SetId(id);
+
+          conn.Close();
+          if (!(conn == null))
+          {
+            conn.Dispose();
+          }
+          return myUser;
+        }
+        public static void Update(string name,string login,string password,string email, int roomNumber, int characterId, int userId)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"UPDATE users SET login=@login, name = @name, password = @password, email = @email, room_number = @room, character_id = @ characterId WHERE id = @userId;";
+
+            MySqlParameter tempName = new MySqlParameter("@name", name);
+            MySqlParameter tempLogin = new MySqlParameter("@login", login);
+            MySqlParameter tempPassword = new MySqlParameter("@password", password);
+            MySqlParameter tempEmail = new MySqlParameter("@email", email);
+            MySqlParameter tempRoom = new MySqlParameter("@room", roomNumber);
+            MySqlParameter tempCharacterId = new MySqlParameter("@characterId", characterId);
+            MySqlParameter tempUserId = new MySqlParameter("@userId", userId);
+
+            cmd.Parameters.Add(tempName);
+            cmd.Parameters.Add(tempLogin);
+            cmd.Parameters.Add(tempPassword);
+            cmd.Parameters.Add(tempEmail);
+            cmd.Parameters.Add(tempRoom);
+            cmd.Parameters.Add(tempCharacterId);
+            cmd.Parameters.Add(tempUserId);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+        public static void Delete(int idDelete)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM users WHERE id = @searchId;";
+
+            MySqlParameter searchId = new MySqlParameter();
+            searchId.ParameterName = "@searchId";
+            searchId.Value = idDelete;
+            cmd.Parameters.Add(searchId);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
         public Character GetCharacter()
         {
             MySqlConnection conn = DB.Connection();
